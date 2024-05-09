e502a96b3aec05d0411c603ee94a72745d50944a
MarinaaMaged <125571149+MarinaaMaged@users.noreply.github.com>
2024-05-09 13:51:05 +0300

    Add files via upload

---
+++ supermarketproject/SupermarketProject.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarketproject;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author N A N A
+ */
+public class SupermarketProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //registration 
+       
+    }
+    
+}
